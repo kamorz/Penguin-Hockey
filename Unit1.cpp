1f41981194febd6f802e536173eb7ec077ed5931
Kamil Orzechowski <kamil.orzechowski.programista@gmail.com>
2019-10-26 15:50:02 +0200

    Added fluent moving of ball

--- Unit1.cpp
+++ Unit1.cpp
@@ -7,12 +7,49 @@
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
-TForm1 *Form1;
+TPenguinHockey *PenguinHockey;
+int verticalMove=6;
+int horizontalMove=-6;
+
+int playerGoals=0;
+int opponentGoals=0;
+
 //---------------------------------------------------------------------------
-__fastcall TForm1::TForm1(TComponent* Owner)
+__fastcall TPenguinHockey::TPenguinHockey(TComponent* Owner)
         : TForm(Owner)
+
 {
 }
+//---------------------------------------------------------------------------
+
+
+
+void __fastcall TPenguinHockey::ballMoveTimer(TObject *Sender)
+{
+   ball->Left+=horizontalMove;
+   ball->Top+=verticalMove;
+
+   if (ball->Left-5  <= background->Left)       horizontalMove=-horizontalMove;
+   if (ball->Top-5   <= background->Top)        verticalMove=-verticalMove;
+   if (ball->Left +ball->Width+5 >= background->Width) horizontalMove=-horizontalMove;
+   if (ball->Top+ ball->Height>= background->Top+background->Height)    verticalMove=-verticalMove;
+
+   //OPPONENT GOAL
+   if (
+   verticalMove>=0 &&
+   ball->Top>= playerTarget->Top-15 &&
+   ball->Top<= playerTarget->Top &&
+   ball->Left  >= playerTarget->Left &&
+   ball->Left +ball->Width <= playerTarget->Left+playerTarget->Width)
+   {
+    ballMove->Enabled=false;
+    opponentGoals+=1;
+    Label5->Caption=playerGoals;
+    ball->Visible=false;
+
+   }
+
+   }
 //---------------------------------------------------------------------------
 
 
